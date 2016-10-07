93adce43d2fae7fa5e0cb787d0cb145f710e920d
Josh Gribbon <jgribbon@stevens.edu>
2016-10-06 20:47:11 -0400

    All views except the add event form now have html and most of the style

--- cs554/assignments/01_calendar/routes/index.js
+++ cs554/assignments/01_calendar/routes/index.js
@@ -9,9 +9,29 @@ const constructorMethod = (app) => {
         //and only get data in that scope
         let weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
         res.render("layouts/month", {
-            pageTitle: "Calendar", 
+            pageTitle: "Month View", 
             month: month, 
             weekDays:weekDays
+        });
+    });
+    app.get("/day", (req, res) => {
+        let month = "October";
+        //maybe get days of the month and everything here then deliver it
+        //splt up routes to use /month/<number> /year/month/day/<number> etc
+        //and only get data in that scope
+        res.render("layouts/day", {
+            pageTitle: "Day View", 
+            month: month
+        });
+    });
+    app.get("/event", (req, res) => {
+        let month = "October";
+        //maybe get days of the month and everything here then deliver it
+        //splt up routes to use /month/<number> /year/month/day/<number> etc
+        //and only get data in that scope
+        res.render("layouts/event", {
+            pageTitle: "Event View", 
+            month: month
         });
     });
     
